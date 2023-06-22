f67ec5de12392a56fb4d2844798932fec699b478
Trọng Nghĩa <ptnghia3502@gmail.com>
2023-06-22 16:19:45 +0700

    fetch data explore

--- GroupStudyUI/Pages/Explore.cshtml.cs
+++ GroupStudyUI/Pages/Explore.cshtml.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.IService;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +10,18 @@ namespace GroupStudyUI.Pages
 {
     public class ExploreModel : PageModel
     {
-        public void OnGet()
+		private readonly IGroupService _groupService;
+		private readonly IHttpContextAccessor _contextAccessor;
+		public ExploreModel(IGroupService groupService, IHttpContextAccessor contextAccessor)
+		{
+			_groupService = groupService;
+			_contextAccessor = contextAccessor;
+		}
+        public List<Group> listGroups { get; set; }
+        public async Task OnGet()
         {
+            IEnumerable<Group> groups = await _groupService.GetAllGroup();
+            listGroups = new List<Group>(groups);
         }
     }
 }
